5937c925f2e8f156de3066518b55247733652584
Artur <artur.sarkisjan@outlook.de>
2021-01-15 10:30:25 +0100

    ingress added

--- kube-server/src/main/java/com/template/kube/controller/PersonController.java
+++ kube-server/src/main/java/com/template/kube/controller/PersonController.java
@@ -7,7 +7,6 @@ import com.template.kube.repository.PersonRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +14,11 @@ public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @GetMapping("/test")
+    public String get() {
+        return "Hallo, Chris!";
+    }
 
     @GetMapping("/persons")
     public List<Person> getAllPersons() {
